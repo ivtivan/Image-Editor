9f4e1833d7b59cb2ac3f7c9ad257f2fd20bc3399
ivtivan <48709677+ivtivan@users.noreply.github.com>
2021-06-04 16:01:52 +0300

    dither setup

--- ImageEditors/Dither/Dither.cpp
+++ ImageEditors/Dither/Dither.cpp
@@ -4,10 +4,30 @@ Dither::Dither(std::string algorithmName) : algorithmName(algorithmName) {
 
 }
 
+void Dither::setUpDistributionMatrix(std::size_t dMatrixRows, std::size_t dMatrixCols, std::size_t pos) {
+    this->dMatrixRows = dMatrixRows;
+    this->dMatrixCols = dMatrixCols;
+    this->pos = pos;
+}
+
+void Dither::setUpErrorMatrix(std::size_t eMatrixRows) {
+    this->eMatrixRows = eMatrixRows;
+    this->errorMatrix[0] = new int[this->eMatrixCols];
+
+    if (this->eMatrixRows == 2) {
+        try {
+            this->errorMatrix[1] = new int[this->eMatrixCols];
+        }
+        catch(const bad_alloc&) {
+            delete[] errorMatrix[0];
+        }
+    }
+}
+
 void Dither::setUpFS() {
-    this->rows = 2;
-    this->cols = 3;
-    this->pos = 1;
+    setUpErrorMatrix(2);
+    setUpDistributionMatrix(2, 3, 1);
+
     this->dMatrix[0][2] = 7;
     this->dMatrix[1][0] = 3;
     this->dMatrix[1][1] = 5;
@@ -16,9 +36,9 @@ void Dither::setUpFS() {
 }
 
 void Dither::setUpJJN() {
-    this->rows = 3;
-    this->cols = 5;
-    this->pos = 2;
+    setUpErrorMatrix(2);
+    setUpDistributionMatrix(3, 5, 2);
+
     this->dMatrix[0][3] = 7;
     this->dMatrix[0][4] = 5;
     this->dMatrix[1][0] = 3;
@@ -34,42 +54,136 @@ void Dither::setUpJJN() {
     this->errorDivisor = 48;
 }
 
+void Dither::setUpStucki() {
+    setUpErrorMatrix(2);
+    setUpDistributionMatrix(3, 5, 2);
+    
+    this->dMatrix[0][3] = 8;
+    this->dMatrix[0][4] = 4;
+    this->dMatrix[1][0] = 2;
+    this->dMatrix[1][1] = 4;
+    this->dMatrix[1][2] = 8;
+    this->dMatrix[1][3] = 4;
+    this->dMatrix[1][4] = 2;
+    this->dMatrix[2][0] = 1;
+    this->dMatrix[2][1] = 2;
+    this->dMatrix[2][2] = 4;
+    this->dMatrix[2][3] = 2;
+    this->dMatrix[2][4] = 1;
+    this->errorDivisor = 42;
+}
+
+void Dither::setUpAtkinson() {
+    setUpErrorMatrix(2);
+    setUpDistributionMatrix(3, 4, 1);
+
+    this->dMatrix[0][2] = 1;
+    this->dMatrix[0][3] = 1;
+    this->dMatrix[1][0] = 1;
+    this->dMatrix[1][1] = 1;
+    this->dMatrix[1][2] = 1;
+    this->dMatrix[2][1] = 1;
+    this->errorDivisor = 8;
+}
+
+void Dither::setUpBurkes() {
+    setUpErrorMatrix(1);
+    setUpDistributionMatrix(2, 5, 2);
+
+    this->dMatrix[0][3] = 8;
+    this->dMatrix[0][4] = 4;
+    this->dMatrix[1][0] = 2;
+    this->dMatrix[1][1] = 4;
+    this->dMatrix[1][2] = 8;
+    this->dMatrix[1][3] = 4;
+    this->dMatrix[1][4] = 2;
+    this->errorDivisor = 32;
+}
+
+void Dither::setUpSierra() {
+    setUpErrorMatrix(2);
+    setUpDistributionMatrix(3, 5, 2);
+
+    this->dMatrix[0][3] = 5;
+    this->dMatrix[0][4] = 3;
+    this->dMatrix[1][0] = 2;
+    this->dMatrix[1][1] = 4;
+    this->dMatrix[1][2] = 5;
+    this->dMatrix[1][3] = 4;
+    this->dMatrix[1][4] = 2;
+    this->dMatrix[2][1] = 2;
+    this->dMatrix[2][2] = 3;
+    this->dMatrix[2][3] = 2;
+    this->errorDivisor = 32;
+}
+
+void Dither::setUpTRSierra() {
+    setUpErrorMatrix(1);
+    setUpDistributionMatrix(2, 5, 2);
+    this->dMatrix[0][3] = 4;
+    this->dMatrix[0][4] = 3;
+    this->dMatrix[1][0] = 1;
+    this->dMatrix[1][1] = 2;
+    this->dMatrix[1][2] = 3;
+    this->dMatrix[1][3] = 2;
+    this->dMatrix[1][4] = 1;
+    this->errorDivisor = 16;
+}
+
+void Dither::setUpSieraLite() {
+    setUpErrorMatrix(1);
+    setUpDistributionMatrix(2, 3, 1);
+
+    this->dMatrix[0][2] = 2;
+    this->dMatrix[1][0] = 1;
+    this->dMatrix[1][1] = 1;
+    this->errorDivisor = 4;
+}
+
+void Dither::setUp4Bayer() {
+    // TODO
+}
+
+void Dither::setUp8Bayer() {
+    // TODO
+}
+
 void Dither::setUpDither(Image* image) {
-    errorDivisor = new int[image->getcols()];
+    eMatrixesCols = image->getCols();
 
     if (this->algorithmName == "FS") {
-
+        setUpFS();
     }
     else if (this->algorithmName == "JJN") {
-
+        setUpJJN();
     }
     else if (this->algorithmName == "Stucki") {
-
+        setUpStucki();
     }
     else if (this->algorithmName == "Atkinson") {
-
+        setUpAtkinson();
     }
     else if (this->algorithmName == "Burkes") {
-
+        setUpBurkes();
     }
     else if (this->algorithmName == "Sierra") {
-
+        setUpSierra();
     }
     else if (this->algorithmName == "TRSierra") {
-
+        setUpTRSierra();
     }
     else if (this->algorithmName == "SieraLite") {
-
+        setUpSieraLite();
     }
     else if (this->algorithmName == "4Bayer") {
-
+        setUp4Bayer();
     }
     else if (this->algorithmName == "8Bayer") {
-
+        setUp8Bayer();
     }
 }
 
-void Dither::DitherImage(Image* image) {
+void Dither::ditherImage(Image* image) {
     setUpDither();
 
     for (std::size_t i = 0; i < image->getRows(); ++i) {
@@ -80,5 +194,7 @@ void Dither::DitherImage(Image* image) {
 }
 
 Dither::~Dither() {
-    delete this->nextRowErrors;
+    for (std::size_t i = 0; i < MAX_ERROR_MATRIX_ROWS; ++i) {
+        delete[] this->errorMatrix[i];
+    }
 }
--- ImageEditors/Dither/Dither.h
+++ ImageEditors/Dither/Dither.h
@@ -8,15 +8,21 @@ class Dither {
 
         const std::size_t MAX_DISTRIBUTION_MATRIX_ROWS = 3;
         const std::size_t MAX_DISTRIBUTION_MATRIX_COLS = 5;
+        const std::size_t MAX_ERROR_MATRIX_ROWS = 3;
 
         std::size_t dMatrixRows;
         std::size_t dMatrixCols;
 
         unsigned int pos;
         unsigned int dMatrix[MAX_DISTRIBUTION_MATRIX_ROWS][MAX_DISTRIBUTION_MATRIX_COLS] = { 0 };
-        unsigned int errorDivisor;
+        unsigned int distributionDivisor;
 
-        int nextRowErrors*;
+        int* errorMatrix[MAX_ERROR_MATRIX_ROWS];
+        std::size_t eMatrixRows;
+        std::size_t eMatrixesCols;
+
+        void setUpDistributionMatrix(std::size_t dMatrixRows, std::size_t dMatrixCols, std::size_t pos);
+        void setUpErrorMatrix(std::size_t eMatrixRows);
 
         void setUpFS();
         void setUpJJN();
@@ -33,7 +39,7 @@ class Dither {
     public:
         Dither(std::string algorithmName);
 
-        void DitherImage(Image* image);
+        void ditherImage(Image* image);
 
         ~Dither();
 };
