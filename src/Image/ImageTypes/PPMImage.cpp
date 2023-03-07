d3623e28baf01520099836feac653d52728edb4c
Ivan <48709677+ivtivan@users.noreply.github.com>
2023-03-07 23:21:39 +0100

    abomination

--- src/Image/ImageTypes/PPMImage.cpp
+++ src/Image/ImageTypes/PPMImage.cpp
@@ -7,18 +7,27 @@ PPMImage::PPMImage(const std::ifstream& sourceFile) : Image(sourceFile) {
 PPMImage::PPMImage(std::string hexColor, std::size_t rows, std::size_t cols) {
     this->rows = rows;
     this->cols = cols;
+    
+    allocatePixels();
 }
 
 void PPMImage::allocatePixels() {
+    // placement-new used, care with destructor
+    // TODO: TEST!!!!!
     pixels = new PPMPixel*[rows];
     for (std::size_t row = 0; row < rows; ++row) {
-        pixels[row] = new PPMPixel[cols];
+        pixels[row] = (PPMPixel*) operator new[](cols * sizeof(PPMPixel));
     }
 }
 
 PPMImage::~PPMImage() {
+    PPMPixel* toDelete;
     for (std::size_t row = 0; row < rows; ++row) {
-        delete[] pixels[row];
+        toDelete = static_cast<PPMPixel*>(pixels[row]);
+        for (std::size_t col = 0; col < cols; ++col) {
+            toDelete[col].~PPMPixel();
+        }
+        operator delete[](pixels[row]);
     }
     delete[] pixels;
 }
